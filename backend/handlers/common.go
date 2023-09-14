6ffafe80e300103293c31a0c4fc122796789efb9
can <21026424+nzmcno@users.noreply.github.com>
2023-09-14 19:04:44 +0100

    components moved to the folder

--- backend/handlers/common.go
+++ backend/handlers/common.go
@@ -13,18 +13,18 @@ func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 
 	// Define the paths to the layout, header, footer, and the specific template
 	layoutPath := filepath.Join("frontend", "layout.html")
-	headerPath := filepath.Join("frontend", "header.html")
-	footerPath := filepath.Join("frontend", "footer.html")
+	headerPath := filepath.Join("frontend", "components/header.html")
+	footerPath := filepath.Join("frontend", "components/footer.html")
 	templatePath := filepath.Join("frontend", tmpl+".html")
 
 	// Add new one to here with if else $If you come from main go and added new page$
 	// If you created only one html file for your page no need this area for more components
 	if tmpl == "index" {
-		musicCardPath := filepath.Join("frontend", "music_card.html")
+		musicCardPath := filepath.Join("frontend", "components/music_card.html")
 		t = templateParseFiles(w, layoutPath, headerPath, footerPath, templatePath, musicCardPath)
 	} else if tmpl == "group" {
-		googleMapsPath := filepath.Join("frontend", "google_maps.html")
-		groupInfoPath := filepath.Join("frontend", "group_info.html")
+		googleMapsPath := filepath.Join("frontend", "components/google_maps.html")
+		groupInfoPath := filepath.Join("frontend", "components/group_info.html")
 		t = templateParseFiles(w, layoutPath, headerPath, footerPath, templatePath, googleMapsPath, groupInfoPath)
 	} else {
 		t = templateParseFiles(w, layoutPath, headerPath, footerPath, templatePath)
