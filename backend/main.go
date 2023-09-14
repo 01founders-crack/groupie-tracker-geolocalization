421c8136d6e3da7d294aff6ad5efc6178f682d2e
can <21026424+nzmcno@users.noreply.github.com>
2023-09-14 19:30:16 +0100

    robots.txt added

--- backend/main.go
+++ backend/main.go
@@ -17,6 +17,10 @@ func main() {
 	// Serve static files and set up routes
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/styles"))))
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./frontend/images"))))
+
+	http.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "frontend/robots.txt")
+	})
 	http.HandleFunc("/", handlers.HandleRoot)
 	http.HandleFunc("/group", handlers.HandleGroup)
 	http.HandleFunc("/500", handlers.Handle500)
